798425066530818a4ff34879632f9d5f3aacd783
fluxytux <amekonnen@newtech.coppellisd.com>
2022-03-30 15:47:00 -0500

    dfg

--- src/labs/PerformanceTask.java
+++ src/labs/PerformanceTask.java
@@ -6,15 +6,18 @@
 	
 		public static void main(String[] args) {
 			
-		Scanner inKey1 = new Scanner(System.in);									
+		Scanner inKey1 = new Scanner(System.in);		
+		//Asks for launch velocity and angle
 		System.out.print("Enter launch velocity (m/s) >>> ");	
 		int launchVelocity = inKey1.nextInt();
 		System.out.print("Enter launch angle (degrees) >>> ");	
 		int launchAngle = inKey1.nextInt();	
+		//prints "projectile path
 		System.out.println("Projectile's path: ");
-				
+		//integers for time, y position, and x positions
 		int time=0;
 		int yPos = 0;
+		//calculates x and y positions with decimals 
 		double xPos=Math.cos(Math.toRadians(launchAngle)) * time * launchVelocity;
 		double ypos=Math.sin(Math.toRadians(launchAngle)) * time * launchVelocity - 0.5 * 9.8 * Math.pow(launchAngle, launchVelocity);
 		boolean launch = true;
@@ -24,7 +27,7 @@
 		
 		while (launch) {
 		
-		
+		//adds time to equation calculates velocity w/ time
 		     time++;
 		     xPos=Math.cos(Math.toRadians(launchAngle)) * time * launchVelocity;
 		     yPos=Math.sin(Math.toRadians(launchAngle)) * time * launchVelocity - 0.5 * 9.8 * Math.pow(ypos, ypos);
